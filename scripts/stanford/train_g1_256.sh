f35445ccce5c637a9798a040f84f7d458df4ba16
david <davidmiller252@gmail.com>
2019-03-14 17:44:35 -0400

    added stanford train script

---
+++ scripts/stanford/train_g1_256.sh
@@ -0,0 +1,5 @@
+python train.py --name edge2face_256_g1 \
+--dataroot datasets/stanford_campus_dataset/ --dataset_mode stanford \
+--input_nc 6 --loadSize 256 --ngf 64 \
+--max_frames_per_gpu 6 --n_frames_total 12 \
+--niter 20 --niter_decay 20
