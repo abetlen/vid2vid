4526ab737d6f6fd2c225142666f56fdb10de4c15
david <davidmiller252@gmail.com>
2019-03-20 17:13:52 -0400

    set no first img and correct output_nc

--- scripts/kaist/train_g1_256_ir2rgb.sh
+++ scripts/kaist/train_g1_256_ir2rgb.sh
@@ -2,8 +2,9 @@ python train.py --name kaist_256_g1_ir2rgb \
        --dataroot datasets/stanford_campus_dataset/ \
        --dataset_mode kaist \
        --loadSize 256 --ngf 64 \
-       --input_nc 1 \
+       --input_nc 3 \
        --output_nc 3 \
+       --no_first_img \
        --imgmode ir2rgb \
        --max_frames_per_gpu 6 --n_frames_total 12 \
        --niter 20 --niter_decay 20 \
--- scripts/kaist/train_g1_256_rgb2ir.sh
+++ scripts/kaist/train_g1_256_rgb2ir.sh
@@ -3,8 +3,9 @@ python train.py --name kaist_256_g1_rgb2ir \
        --dataset_mode kaist \
        --loadSize 256 --ngf 64 \
        --input_nc 3 \
-       --output_nc 1 \
+       --output_nc 3 \
        --imgmode rgb2ir \
+       --no_first_img \
        --max_frames_per_gpu 6 --n_frames_total 12 \
        --niter 20 --niter_decay 20 \
        --tf_log
