b2ad4af249c825e2bf21c18e0a10c4afe2031d72
david <davidmiller252@gmail.com>
2019-03-20 15:36:52 -0400

    Added rgb2ir and ir2rgb training scripts

---
+++ scripts/kaist/train_g1_256_ir2rgb.sh
@@ -0,0 +1,10 @@
+python train.py --name stanford_256_g1 \
+       --dataroot datasets/stanford_campus_dataset/ \
+       --dataset_mode kaist \
+       --loadSize 256 --ngf 64 \
+       --input_nc 1 \
+       --output_nc 3 \
+       --mode ir2rgb \
+       --max_frames_per_gpu 6 --n_frames_total 12 \
+       --niter 20 --niter_decay 20 \
+       --tf_log
