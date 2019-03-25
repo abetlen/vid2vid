c3e4dc0cc75ad30eff9566fc96096d868069efda
david <davidmiller252@gmail.com>
2019-03-25 14:34:57 -0400

    added params to 8 gpu train file

--- scripts/stanford/train_g8_256.sh
+++ scripts/stanford/train_g8_256.sh
@@ -1,12 +1,14 @@
-python train.py --name stanford_256_g1 \
+python train.py --name stanford_256_g8 \
        --dataroot datasets/stanford_campus_dataset/ \
        --dataset_mode stanford \
        --loadSize 256 --ngf 64 \
        --input_nc 6 \
-       --output_nc 3 \
+       --num_D 2 \
        --no_first_img \
+       --fineSize 256
        --gpu_ids 0,1,2,3,4,5,6,7 --n_gpus_gen 6 \
-       --max_frames_per_gpu 6 --n_frames_total 12 \
+       --max_frames_per_gpu 3 --n_frames_total 12 \
        --niter 20 --niter_decay 20 \
+       --batchSize 8 \
        --tf_log
 
