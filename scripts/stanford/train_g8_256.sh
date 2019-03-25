0591b6aef7083f23dfded49f8a44c87db7fac651
david <davidmiller252@gmail.com>
2019-03-25 14:55:52 -0400

    fixed typo

--- scripts/stanford/train_g8_256.sh
+++ scripts/stanford/train_g8_256.sh
@@ -5,7 +5,7 @@ python train.py --name stanford_256_g8 \
        --input_nc 6 \
        --num_D 2 \
        --no_first_img \
-       --fineSize 256
+       --fineSize 256 \
        --gpu_ids 0,1,2,3,4,5,6,7 --n_gpus_gen 6 \
        --max_frames_per_gpu 3 --n_frames_total 12 \
        --niter 20 --niter_decay 20 \
