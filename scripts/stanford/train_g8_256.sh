3d9959a335b49e1a6d20b41ec37bafbe3880e91a
david <davidmiller252@gmail.com>
2019-03-25 14:58:25 -0400

    bugfix

--- scripts/stanford/train_g8_256.sh
+++ scripts/stanford/train_g8_256.sh
@@ -7,7 +7,7 @@ python train.py --name stanford_256_g8 \
        --no_first_img \
        --fineSize 256 \
        --gpu_ids 0,1,2,3,4,5,6,7 --n_gpus_gen 6 \
-       --n_frames_total 12 \
+       --n_frames_total 16 \
        --niter 20 --niter_decay 20 \
        --batchSize 8 \
        --tf_log
