9e5d50c8ccdb8427e2d727d48259e0f60d364516
david <davidmiller252@gmail.com>
2019-03-26 11:47:39 -0400

    tensorboard log

--- scripts/kaist/train_g1_256.sh
+++ scripts/kaist/train_g1_256.sh
@@ -6,4 +6,5 @@ python train.py --name kaist_256_g1_ir2rgb \
        --max_frames_per_gpu 6 \
        --no_first_img \
        --n_frames_total 12 \
-       --niter 20 --niter_decay 20
+       --niter 20 --niter_decay 20 \
+       --tf_log
