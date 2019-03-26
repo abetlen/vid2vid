2ed09ef185a49b85d677c9b6e1d1ba9a539c468a
david <davidmiller252@gmail.com>
2019-03-26 15:26:00 -0400

    test script updated

--- scripts/kaist/test_g1_256.sh
+++ scripts/kaist/test_g1_256.sh
@@ -1,10 +1,12 @@
-python test.py --name kaist_256_g1_ir2rgb \
+python test_custom.py --name kaist_256_g1_ir2rgb \
        --dataroot /home/ubuntu/datasets/images/ \
+       --dataset_mode kaist \
        --loadSize 256 --ngf 64 \
        --fineSize 256 \
        --input_nc 3 \
        --output_nc 3 \
        --no_first_img \
+       --n_frames_total 12 \
        --imgmode ir2rgb \
-       --how_many 50 \
+       --how_many 2 \
 
