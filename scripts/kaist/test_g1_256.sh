2f5ec3a235be739a4919550e8b3ee9fa528061cc
david <davidmiller252@gmail.com>
2019-03-26 17:01:38 -0400

    test2

--- scripts/kaist/test_g1_256.sh
+++ scripts/kaist/test_g1_256.sh
@@ -1,7 +1,7 @@
 python test.py --name kaist_256_g1_ir2rgb \
        --dataroot /home/ubuntu/datasets/images/ \
        --dataset_mode kaist_test \
-       --loadSize 256 --ngf 64 \
+       --loadSize 512 --ngf 64 \
        --fineSize 256 \
        --input_nc 3 \
        --output_nc 3 \
