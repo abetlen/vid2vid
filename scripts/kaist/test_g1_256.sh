0ebeab87be2284e42ad6455e8975b55dc4ecfbcb
david <davidmiller252@gmail.com>
2019-03-26 10:52:08 -0400

    trying default dataset mode

--- scripts/kaist/test_g1_256.sh
+++ scripts/kaist/test_g1_256.sh
@@ -1,10 +1,10 @@
 python test.py --name kaist_256_g1_ir2rgb \
        --dataroot datasets/stanford_campus_dataset/ \
-       --dataset_mode kaist \
        --loadSize 256 --ngf 64 \
        --input_nc 3 \
        --output_nc 3 \
        --no_first_img \
        --imgmode ir2rgb \
-       --how_many 10
+       --how_many 10 \
+
 
