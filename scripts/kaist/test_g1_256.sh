9c32809563a8e3b9f3d3b3c6dfb405a868eef3af
david <davidmiller252@gmail.com>
2019-03-26 11:32:49 -0400

    aoeu

--- scripts/kaist/test_g1_256.sh
+++ scripts/kaist/test_g1_256.sh
@@ -1,5 +1,5 @@
 python test.py --name kaist_256_g1_ir2rgb \
-       --dataroot datasets/stanford_campus_dataset/ \
+       --dataroot datasets/images/ \
        --loadSize 256 --ngf 64 \
        --input_nc 3 \
        --output_nc 3 \
