01af05635116122fd7ba9ac625499f39d2f2546f
david <davidmiller252@gmail.com>
2019-03-27 17:16:59 -0400

    8 gpu training for kaist set up

--- scripts/kaist/train_g8_256_ir2rgb.sh
+++ scripts/kaist/train_g8_256_ir2rgb.sh
@@ -6,7 +6,9 @@ python train.py --name kaist_256_g8_ir2rgb \
        --output_nc 3 \
        --no_first_img \
        --imgmode ir2rgb \
-       --gpu_ids 0,1,2,3,4,5,6,7 --n_gpus_gen 6 \
-       --max_frames_per_gpu 6 --n_frames_total 12 \
+       --gpu_ids 0,1,2,3,4,5,6,7 \
+       --batchSize 8 \
+       --n_frames_total 12 \
        --niter 20 --niter_decay 20 \
-       --tf_log
+       --tf_log \
+       --continue_train
