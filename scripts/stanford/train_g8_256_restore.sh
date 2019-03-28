bb671d35a74ee1c922692cd47076d81c75237369
david <davidmiller252@gmail.com>
2019-03-28 11:10:13 -0400

    added continue train script for stanford

---
+++ scripts/stanford/train_g8_256_restore.sh
@@ -0,0 +1,15 @@
+python train.py --name stanford_256_g8 \
+       --dataroot datasets/stanford_campus_dataset/ \
+       --dataset_mode stanford \
+       --loadSize 256 --ngf 64 \
+       --input_nc 6 \
+       --num_D 2 \
+       --no_first_img \
+       --fineSize 256 \
+       --gpu_ids 0,1,2,3,4,5,6,7 \
+       --n_frames_total 12 \
+       --niter 20 --niter_decay 20 \
+       --batchSize 8 \
+       --tf_log \
+       --continue_train
+
