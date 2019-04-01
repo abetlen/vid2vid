ee1fb5cf80c6fb73c4776ece21beec008fba868a
david <davidmiller252@gmail.com>
2019-04-01 10:37:22 -0400

    Added g8 test script

---
+++ scripts/kaist/test_g8_256.sh
@@ -0,0 +1,10 @@
+python test.py --name kaist_256_g8_ir2rgb \
+       --dataroot /home/ubuntu/datasets/images/ \
+       --dataset_mode kaist_test \
+       --loadSize 512 --ngf 64 \
+       --fineSize 256 \
+       --input_nc 3 \
+       --output_nc 3 \
+       --no_first_img \
+       --imgmode ir2rgb \
+       --how_many 50 \
