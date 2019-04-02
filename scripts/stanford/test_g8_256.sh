0c7f10c9886d1ded65b61484b1a00b504860e821
david <davidmiller252@gmail.com>
2019-04-01 21:05:37 -0400

    added g8 script

---
+++ scripts/stanford/test_g8_256.sh
@@ -0,0 +1,9 @@
+python test.py --name stanford_256_g8 \
+       --dataroot datasets/stanford_campus_dataset/ \
+       --dataset_mode stanford_test \
+       --input_nc 6 \
+       --loadSize 1400 \
+       --ngf 64 \
+       --max_frames_per_gpu 6 \
+       --no_first_img \
+       --how_many 50
