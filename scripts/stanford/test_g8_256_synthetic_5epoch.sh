803e526088bedf25e26fab324afeb880fb11e5a0
david <davidmiller252@gmail.com>
2019-04-05 15:31:16 -0400

    Added inference script for younger model

---
+++ scripts/stanford/test_g8_256_synthetic_5epoch.sh
@@ -0,0 +1,8 @@
+python test.py --name stanford_256_g8_5_epochs \
+       --dataroot datasets/stanford_campus_dataset/ \
+       --dataset_mode stanford_synthetic \
+       --input_nc 6 \
+       --loadSize 1400 \
+       --ngf 64 \
+       --no_first_img \
+       --how_many 3000
